53b03d29bf3b3a09e56662aa36aa045ad1b7a07d
Geoyeob Kim <gasbank@msn.com>
2017-04-19 01:02:35 +0900

    LWTIMEPOINT

--- src/lwtimepoint.c
+++ src/lwtimepoint.c
@@ -1,22 +1,22 @@
 #include "lwtimepoint.h"
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
+#if LW_TIMESPEC_AVAILABLE
+// Nothing
+#else
 #include "GLFW/glfw3.h"
 #endif
 
 void lwtimepoint_now(LWTIMEPOINT* tp) {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-	tp->last_time = glfwGetTime();
-#else
+#if LW_TIMESPEC_AVAILABLE
 	struct timespec now;
 	clock_gettime(CLOCK_MONOTONIC, &now);
 	tp->last_time = now;
+#else
+	tp->last_time = glfwGetTime();
 #endif
 }
 
 double lwtimepoint_diff(const LWTIMEPOINT* a, const LWTIMEPOINT* b) {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-	return a->last_time - b->last_time;
-#else
+#if LW_TIMESPEC_AVAILABLE
 	long nsec_diff = a->last_time.tv_nsec - b->last_time.tv_nsec;
 	long sec_diff = a->last_time.tv_sec - b->last_time.tv_sec;
 
@@ -26,21 +26,23 @@ double lwtimepoint_diff(const LWTIMEPOINT* a, const LWTIMEPOINT* b) {
 	}
 
 	return sec_diff + (double)nsec_diff / 1e9;
+#else
+	return a->last_time - b->last_time;
 #endif
 }
 
 long lwtimepoint_get_second_portion(const LWTIMEPOINT* tp) {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-	return (int)tp->last_time;
-#else
+#if LW_TIMESPEC_AVAILABLE
 	return (long)tp->last_time.tv_sec;
+#else
+	return (int)tp->last_time;
 #endif
 }
 
 long lwtimepoint_get_nanosecond_portion(const LWTIMEPOINT* tp) {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-	return (long)((tp->last_time - (int)tp->last_time) * 1e9);
-#else
+#if LW_TIMESPEC_AVAILABLE
 	return (long)tp->last_time.tv_nsec;
+#else
+	return (long)((tp->last_time - (int)tp->last_time) * 1e9);
 #endif
 }
--- src/lwtimepoint.h
+++ src/lwtimepoint.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "platform_detection.h"
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-#else
+
+#define LW_TIMESPEC_AVAILABLE !(LW_PLATFORM_WIN32 || LW_PLATFORM_OSX)
+
+#if LW_TIMESPEC_AVAILABLE
 #include <time.h> // ios struct timespec
 #endif
 
 typedef struct _LWTIMEPOINT {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-	double last_time;
-#else
+#if LW_TIMESPEC_AVAILABLE
 	struct timespec last_time;
+#else
+	double last_time;
 #endif
 } LWTIMEPOINT;
 
