2dab605f09a50c6130d87fc49b292a3fa61e1964
Geoyeob Kim <gasbank@msn.com>
2017-11-15 01:43:03 +0900

    hhh

--- assets-update-sync.py
+++ assets-update-sync.py
@@ -3,6 +3,8 @@ import posixpath
 import hashlib
 import binascii
 import subprocess
+import sys
+import os
 
 def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
     for block in bytesiter:
@@ -15,7 +17,6 @@ def file_as_blockiter(afile, blocksize=65536):
         while len(block) > 0:
             yield block
             block = afile.read(blocksize)
-
 
 def update_list():
 	print('Update list file (recalculating hashes)...')
@@ -38,9 +39,11 @@ def update_list():
 
 def sync_s3():
 	print('Syncing s3...')
-	subprocess.call(['aws.cmd', 's3', 'sync', 'assets', 's3://sky.popsongremix.com/laidoff/assets', '--acl', 'public-read'])
+	subprocess.call(['aws.cmd' if os.name == 'nt' else 'aws', 's3', 'sync', 'assets', 's3://sky.popsongremix.com/laidoff/assets', '--acl', 'public-read'])
 		
 if __name__ == '__main__':
+	if sys.version_info[0] < 3:
+		raise Exception("Python 3 or a more recent version is required.")
 	update_list()
 	sync_s3()
 	
