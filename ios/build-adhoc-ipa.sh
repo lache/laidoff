46ae7641a096a0fa7797c7e92d38a240eb9b6462
Geoyeob Kim <gasbank@msn.com>
2017-12-05 16:24:01 +0900

    ios build script

--- ios/build-adhoc-ipa.sh
+++ ios/build-adhoc-ipa.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 security unlock-keychain -p ${!KEYCHAIN_PASSWORD}
-xcodebuild -scheme laidoff archive -archivePath build/laidoff
+xcodebuild -scheme laidoff clean archive -archivePath build/laidoff
 xcodebuild -exportArchive -exportOptionsPlist exportoptions.plist -archivePath "build/laidoff.xcarchive" -exportPath "build/laidoff.ipa"
-
