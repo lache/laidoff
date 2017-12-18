12f625e61eb4095ea7c2f2e493d7e8c15778f56a
Geoyeob Kim <gasbank@msn.com>
2017-12-18 19:44:31 +0900

    lazy resource loading wip

--- src/laidoff.c
+++ src/laidoff.c
@@ -171,177 +171,7 @@ set_texture_parameter(const LWCONTEXT* pLwc, LWENUM _LW_ATLAS_ENUM lae, LWENUM _
     );
 }
 
-static void delete_shader(LWSHADER *pShader) {
-    glDeleteProgram(pShader->program);
-    pShader->program = 0;
-    glDeleteShader(pShader->vertex_shader);
-    pShader->vertex_shader = 0;
-    glDeleteShader(pShader->fragment_shader);
-    pShader->fragment_shader = 0;
-}
-
-static void
-create_shader(const char *shader_name, LWSHADER *pShader, const GLchar *vst, const GLchar *fst) {
-#if LW_PLATFORM_WIN32 || LW_PLATFORM_OSX
-#define LW_GLSL_VERSION_STATEMENT "#version 150\n"
-#else
-#define LW_GLSL_VERSION_STATEMENT "#version 100\n"
-#endif
-    pShader->valid = 0;
-
-    pShader->vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-
-    const GLchar* vstarray[] = { LW_GLSL_VERSION_STATEMENT, vst };
-    glShaderSource(pShader->vertex_shader, 2, vstarray, NULL);
-    glCompileShader(pShader->vertex_shader);
-    GLint isCompiled = 0;
-    glGetShaderiv(pShader->vertex_shader, GL_COMPILE_STATUS, &isCompiled);
-    if (isCompiled == GL_FALSE) {
-        GLint maxLength = 0;
-        glGetShaderiv(pShader->vertex_shader, GL_INFO_LOG_LENGTH, &maxLength);
-
-        // The maxLength includes the NULL character
-        GLchar *errorLog = (GLchar *)calloc(maxLength, sizeof(GLchar));
-        glGetShaderInfoLog(pShader->vertex_shader, maxLength, &maxLength, errorLog);
-        LOGE("'%s' Vertex Shader Error (length:%d): %s", shader_name, maxLength, errorLog);
-        free(errorLog);
-
-        // Provide the info log in whatever manor you deem best.
-        // Exit with failure.
-        glDeleteShader(pShader->vertex_shader); // Don't leak the shader.
-        return;
-    }
-
-    // fragment shader
-
-    pShader->fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    const GLchar* fstarray[] = { LW_GLSL_VERSION_STATEMENT, fst };
-    glShaderSource(pShader->fragment_shader, 2, fstarray, NULL);
-    glCompileShader(pShader->fragment_shader);
-    glGetShaderiv(pShader->fragment_shader, GL_COMPILE_STATUS, &isCompiled);
-    if (isCompiled == GL_FALSE) {
-        GLint maxLength = 0;
-        glGetShaderiv(pShader->fragment_shader, GL_INFO_LOG_LENGTH, &maxLength);
-
-        // The maxLength includes the NULL character
-        GLchar *errorLog = (GLchar *)calloc(maxLength, sizeof(GLchar));
-        glGetShaderInfoLog(pShader->fragment_shader, maxLength, &maxLength, errorLog);
-        LOGE("'%s' Fragment Shader Error: %s", shader_name, errorLog);
-        free(errorLog);
-
-        // Provide the info log in whatever manor you deem best.
-        // Exit with failure.
-        glDeleteShader(pShader->fragment_shader); // Don't leak the shader.
-        return;
-    }
-
-    pShader->program = glCreateProgram();
-    glAttachShader(pShader->program, pShader->vertex_shader);
-    glAttachShader(pShader->program, pShader->fragment_shader);
-    glLinkProgram(pShader->program);
-    //Note the different functions here: glGetProgram* instead of glGetShader*.
-    GLint isLinked = 0;
-    glGetProgramiv(pShader->program, GL_LINK_STATUS, (int *)&isLinked);
-    if (isLinked == GL_FALSE) {
-        GLint maxLength = 0;
-        glGetProgramiv(pShader->program, GL_INFO_LOG_LENGTH, &maxLength);
-
-        //The maxLength includes the NULL character
-        GLchar *errorLog = (GLchar *)calloc(maxLength, sizeof(GLchar));
-        glGetProgramInfoLog(pShader->program, maxLength, &maxLength, errorLog);
-        LOGE("'%s' Program Link Error: %s", shader_name, errorLog);
-        free(errorLog);
-        //We don't need the program anymore.
-        glDeleteProgram(pShader->program);
-        //Don't leak shaders either.
-        glDeleteShader(pShader->vertex_shader);
-        glDeleteShader(pShader->fragment_shader);
-
-        //Use the infoLog as you see fit.
-
-        //In this simple program, we'll just leave
-        return;
-    }
-
-    glUseProgram(pShader->program);
-    // Uniforms
-    pShader->mvp_location = glGetUniformLocation(pShader->program, "MVP");
-    pShader->vuvoffset_location = glGetUniformLocation(pShader->program, "vUvOffset");
-    pShader->vuvscale_location = glGetUniformLocation(pShader->program, "vUvScale");
-    pShader->vs9offset_location = glGetUniformLocation(pShader->program, "vS9Offset");
-    pShader->m_location = glGetUniformLocation(pShader->program, "M");
-    pShader->alpha_multiplier_location = glGetUniformLocation(pShader->program, "alpha_multiplier");
-    pShader->overlay_color_location = glGetUniformLocation(pShader->program, "overlay_color");
-    pShader->overlay_color_ratio_location = glGetUniformLocation(pShader->program, "overlay_color_ratio");
-    pShader->multiply_color_location = glGetUniformLocation(pShader->program, "multiply_color");
-    pShader->diffuse_location = glGetUniformLocation(pShader->program, "diffuse");
-    pShader->diffuse_arrow_location = glGetUniformLocation(pShader->program, "diffuse_arrow");
-    pShader->alpha_only_location = glGetUniformLocation(pShader->program, "alpha_only");
-    pShader->glyph_color_location = glGetUniformLocation(pShader->program, "glyph_color");
-    pShader->outline_color_location = glGetUniformLocation(pShader->program, "outline_color");
-    pShader->bone_location = glGetUniformLocation(pShader->program, "bone");
-    pShader->rscale_location = glGetUniformLocation(pShader->program, "rscale");
-    pShader->thetascale_location = glGetUniformLocation(pShader->program, "thetascale");
-    pShader->projection_matrix_location = glGetUniformLocation(pShader->program, "uProjectionMatrix");
-    pShader->k_location = glGetUniformLocation(pShader->program, "uK");
-    pShader->color_location = glGetUniformLocation(pShader->program, "uColor");
-    pShader->time_location = glGetUniformLocation(pShader->program, "uTime");
-    pShader->texture_location = glGetUniformLocation(pShader->program, "uTexture");
-    pShader->u_ProjectionViewMatrix = glGetUniformLocation(pShader->program, "u_ProjectionViewMatrix");
-    pShader->u_ModelMatrix = glGetUniformLocation(pShader->program, "u_ModelMatrix");
-    pShader->u_Gravity = glGetUniformLocation(pShader->program, "u_Gravity");
-    pShader->u_Time = glGetUniformLocation(pShader->program, "u_Time");
-    pShader->u_eRadius = glGetUniformLocation(pShader->program, "u_eRadius");
-    pShader->u_eVelocity = glGetUniformLocation(pShader->program, "u_eVelocity");
-    pShader->u_eDecay = glGetUniformLocation(pShader->program, "u_eDecay");
-    pShader->u_eSizeStart = glGetUniformLocation(pShader->program, "u_eSizeStart");
-    pShader->u_eSizeEnd = glGetUniformLocation(pShader->program, "u_eSizeEnd");
-    pShader->u_eScreenWidth = glGetUniformLocation(pShader->program, "u_eScreenWidth");
-    pShader->u_eColorStart = glGetUniformLocation(pShader->program, "u_eColorStart");
-    pShader->u_eColorEnd = glGetUniformLocation(pShader->program, "u_eColorEnd");
-    pShader->u_Texture = glGetUniformLocation(pShader->program, "u_Texture");
-    pShader->u_TextureAlpha = glGetUniformLocation(pShader->program, "u_TextureAlpha");
-    pShader->time = glGetUniformLocation(pShader->program, "time");
-    pShader->resolution = glGetUniformLocation(pShader->program, "resolution");
-    pShader->sphere_pos = glGetUniformLocation(pShader->program, "sphere_pos");
-    pShader->sphere_col = glGetUniformLocation(pShader->program, "sphere_col");
-    pShader->sphere_col_ratio = glGetUniformLocation(pShader->program, "sphere_col_ratio");
-    pShader->sphere_speed = glGetUniformLocation(pShader->program, "sphere_speed");
-    pShader->sphere_move_rad = glGetUniformLocation(pShader->program, "sphere_move_rad");
-    pShader->reflect_size = glGetUniformLocation(pShader->program, "reflect_size");
-    pShader->arrow_center = glGetUniformLocation(pShader->program, "arrow_center");
-    pShader->arrow_angle = glGetUniformLocation(pShader->program, "arrow_angle");
-    pShader->arrow_scale = glGetUniformLocation(pShader->program, "arrow_scale");
-    pShader->arrowRotMat2 = glGetUniformLocation(pShader->program, "arrowRotMat2");
-    pShader->gauge_ratio = glGetUniformLocation(pShader->program, "gauge_ratio");
-    pShader->full_color = glGetUniformLocation(pShader->program, "full_color");
-    pShader->empty_color = glGetUniformLocation(pShader->program, "empty_color");
-    pShader->wrap_offset = glGetUniformLocation(pShader->program, "wrap_offset");
-
-    // Set initial value...
-    glUseProgram(pShader->program);
-    glUniform3f(pShader->multiply_color_location, 1.0f, 1.0f, 1.0f);
-
-    // Attribs
-    pShader->vpos_location = glGetAttribLocation(pShader->program, "vPos");
-    pShader->vcol_location = glGetAttribLocation(pShader->program, "vCol");
-    pShader->vuv_location = glGetAttribLocation(pShader->program, "vUv");
-    pShader->vs9_location = glGetAttribLocation(pShader->program, "vS9");
-    pShader->vbweight_location = glGetAttribLocation(pShader->program, "vBw");
-    pShader->vbmat_location = glGetAttribLocation(pShader->program, "vBm");
-    pShader->theta_location = glGetAttribLocation(pShader->program, "aTheta");
-    pShader->shade_location = glGetAttribLocation(pShader->program, "aShade");
-    pShader->a_pID = glGetAttribLocation(pShader->program, "a_pID");
-    pShader->a_pRadiusOffset = glGetAttribLocation(pShader->program, "a_pRadiusOffset");
-    pShader->a_pVelocityOffset = glGetAttribLocation(pShader->program, "a_pVelocityOffset");
-    pShader->a_pDecayOffset = glGetAttribLocation(pShader->program, "a_pDecayOffset");
-    pShader->a_pSizeOffset = glGetAttribLocation(pShader->program, "a_pSizeOffset");
-    pShader->a_pColorOffset = glGetAttribLocation(pShader->program, "a_pColorOffset");
-
-    pShader->valid = 1;
-}
-
-void init_gl_shaders(LWCONTEXT* pLwc) {
+static void init_gl_shaders(LWCONTEXT* pLwc) {
     lw_create_all_vertex_shader(pLwc);
     lw_create_all_frag_shader(pLwc);
     lw_create_all_shader_program(pLwc);
@@ -540,7 +370,7 @@ static void init_vbo(LWCONTEXT* pLwc) {
     // LVT_TOWER_BASE_2
     load_vbo(pLwc, ASSETS_BASE_PATH "vbo" PATH_SEPARATOR "tower-base-2.vbo",
              &pLwc->vertex_buffer[LVT_TOWER_BASE_2]);
-    
+
     // LVT_LEFT_TOP_ANCHORED_SQUARE ~ LVT_RIGHT_BOTTOM_ANCHORED_SQUARE
     // 9 anchored squares...
     const float anchored_square_offset[][2] = {
@@ -1793,12 +1623,11 @@ LWCONTEXT* lw_init_initial_size(int width, int height) {
     return pLwc;
 }
 
-LWCONTEXT *lw_init(void) {
+LWCONTEXT* lw_init(void) {
     return lw_init_initial_size(0, 0);
 }
 
-int prefix(const char* pre, const char* str)
-{
+int prefix(const char* pre, const char* str) {
     return strncmp(pre, str, strlen(pre)) == 0;
 }
 
