ff1e93e58413ff7ac657532da10ef86da5bd2121
Geoyeob Kim <gasbank@msn.com>
2017-12-20 16:29:52 +0900

    android: enable immersive mode

--- android/app/src/main/java/com/popsongremix/laidoff/LaidoffNativeActivity.java
+++ android/app/src/main/java/com/popsongremix/laidoff/LaidoffNativeActivity.java
@@ -14,6 +14,7 @@ import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -290,6 +291,24 @@ public class LaidoffNativeActivity extends NativeActivity implements RewardedVid
             e.printStackTrace();
         }
         return packageVersionName;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                );
+            }
+        }
     }
 
     // AdMob reward video callbacks
