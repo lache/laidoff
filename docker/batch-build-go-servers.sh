6d48082ca5cab6ba361a9e60ef7df26e9f898241
Geoyeob Kim <gasbank@gmail.com>
2018-04-08 00:46:07 +0900

    dockerize it...

--- docker/batch-build-go-servers.sh
+++ docker/batch-build-go-servers.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
+# laidoff-server (battle-server)
+rm -rf battle
+mkdir battle
+rm -rf build-server
+mkdir build-server
+cd build-server
+CXXFLAGS=-static cmake ../.. -DSERVER_ONLY=1
+make laidoff-server
+cp bin/laidoff-server ../battle
+cd ..
+
+# db-server
 rm -rf db
 CC=/usr/local/musl/bin/musl-gcc go build --ldflags '-w -linkmode external -extldflags "-static"' -o ./db/db-server ../db-server/db.go
+# db-server resources
+cp ../db-server/*.txt db
+cp ../db-server/*.html db
 
+# match-server
 rm -rf match
 CC=/usr/local/musl/bin/musl-gcc go build --ldflags '-w -linkmode external -extldflags "-static"' -o ./match/match-server ../match-server/match.go
+# match-server resources
+cp ../match-server/conf.json.template match/conf.json
 
+# rank-server
 rm -rf rank
 CC=/usr/local/musl/bin/musl-gcc go build --ldflags '-w -linkmode external -extldflags "-static"' -o ./rank/rank-server ../rank-server/rank.go
+# rank-server resources
+# ---
 
+# reward-server
 rm -rf reward
 CC=/usr/local/musl/bin/musl-gcc go build --ldflags '-w -linkmode external -extldflags "-static"' -o ./reward/reward-server ../reward-server/reward.go
+# reward-server resources
+# ---
 
+# push-server
 rm -rf push
 CC=/usr/local/musl/bin/musl-gcc go build --ldflags '-w -linkmode external -extldflags "-static"' -o ./push/push-server ../push-server/push.go
-
+# push-server resources
+cp -r ../push-server/cert push
+cp ../push-server/*.html push
--- docker/runall.sh
+++ docker/runall.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+cd db
 ./db-server >> db.log 2>&1 &
 DB_PID=$!
+cd ..
 sleep 1
+
+cd match
 ./match-server >> match.log 2>&1 &
 MATCH_PID=$!
+cd ..
 sleep 1
+
+cd rank
 ./rank-server >> rank.log 2>&1 &
 RANK_PID=$!
+cd ..
 sleep 1
+
+cd reward
 ./reward-server >> reward.log 2>&1 &
 REWARD_PID=$!
+cd ..
 sleep 1
+
+cd push
 ./push-server >> push.log 2>&1 &
 PUSH_PID=$!
+cd ..
 sleep 1
+
+cd battle
 ./laidoff-server >> laidoff.log 2>&1
 LAIDOFF_PID=$!
 
