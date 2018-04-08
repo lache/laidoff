b01c583922a05c9ec06f824ea875e7bef3dd340e
Geoyeob Kim <gasbank@gmail.com>
2018-04-09 00:22:10 +0900

    docker image: /bin/bash -> /bin/sh

--- docker/scripts/runall.sh
+++ docker/scripts/runall.sh
@@ -1,44 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
-pushd `pwd`
 cd services/db
 ../../bin/br-server db >> db.log 2>&1 &
 DB_PID=$!
-popd
+cd ../..
 sleep 1
 
-pushd `pwd`
 cd services/match
 ../../bin/br-server match >> match.log 2>&1 &
 MATCH_PID=$!
-popd
+cd ../..
 sleep 1
 
-pushd `pwd`
 cd services/rank
 ../../bin/br-server rank >> rank.log 2>&1 &
 RANK_PID=$!
-popd
+cd ../..
 sleep 1
 
-pushd `pwd`
 cd services/reward
 ../../bin/br-server reward >> reward.log 2>&1 &
 REWARD_PID=$!
-popd
+cd ../..
 sleep 1
 
-pushd `pwd`
 cd services/push
 ../../bin/br-server push >> push.log 2>&1 &
 PUSH_PID=$!
-popd
+cd ../..
 sleep 1
 
-pushd `pwd`
 cd services/battle
 ../../bin/laidoff-server >> laidoff.log 2>&1
 # Never reach here...
 LAIDOFF_PID=$!
-popd
+
 
