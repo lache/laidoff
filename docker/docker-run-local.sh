232990849d3c1e2e518ea1661175be36fdf63a49
Geoyeob Kim <gasbank@gmail.com>
2018-04-10 23:47:55 +0900

    add docker localhost run script

---
+++ docker/docker-run-local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run -d \
+	-p 10288:10288/udp \
+	-p 18080:18080 \
+	-p 19856:19856 \
+	-p 20182:20182 \
+	-e "BATTLE_HOST=127.0.0.1" \
+	-v '/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt' \
+	br
+
